bb26ac9cb2994c3afbfa1dee8daec27fc7b0b648
feliperodsdev <libertyisrequired@gmail.com>
2023-01-18 21:33:40 -0300

    fix: path in tests

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -1,46 +1,5 @@
-class LoginRouter {
-  route(httpRequest: any) {
-    if (!httpRequest.body) {
-      return HttpResponse.serverError();
-    }
-    const { username, password } = httpRequest.body;
-    if (!username) {
-      return HttpResponse.badRequest("username");
-    }
-    if (!password) {
-      return HttpResponse.badRequest("password");
-    }
-    return HttpResponse.ok("");
-  }
-}
-
-class HttpResponse {
-  static badRequest(paramName: string) {
-    return {
-      statusCode: 400,
-      body: new MissingParamError(paramName),
-    };
-  }
-  static serverError() {
-    return {
-      statusCode: 500,
-      body: "Internal Server Error",
-    };
-  }
-  static ok(content: any) {
-    return {
-      statusCode: 200,
-      body: content,
-    };
-  }
-}
-
-class MissingParamError extends Error {
-  constructor(paramName: string) {
-    super(`Missing param: ${paramName}`);
-    this.name = "MissingParamError";
-  }
-}
+import LoginRouter from "./Login-router";
+import { MissingParamError } from "./helpers/Missing-param-error";
 
 describe("Login Router", () => {
   it("Should return 400 if no username is provided", () => {

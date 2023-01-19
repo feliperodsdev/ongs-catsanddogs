18b58ce100bb3ec33b8ebb9cdb171d764a243a50
feliperodsdev <libertyisrequired@gmail.com>
2023-01-19 09:49:54 -0300

    feat: moving files

--- src/presentation/routers/Login-router.ts
+++ src/presentation/routers/Login-router.ts
@@ -1,17 +1,29 @@
-import HttpResponse from "./helpers/Http-response";
+import HttpResponse from "../helpers/Http-response";
+import { AuthUserParams } from "./interfaces/authParams";
+import { IAuthService } from "./interfaces/authService";
 
 export default class LoginRouter {
-  route(httpRequest: any) {
+  private authService: IAuthService;
+  constructor(authService: IAuthService) {
+    this.authService = authService;
+  }
+  async route(httpRequest: any) {
     if (!httpRequest.body) {
       return HttpResponse.serverError();
     }
-    const { username, password } = httpRequest.body;
-    if (!username) {
-      return HttpResponse.badRequest("username");
+
+    const requiredFields = ["username", "password"];
+
+    for (const field of requiredFields) {
+      if (
+        !httpRequest?.body?.[field as keyof AuthUserParams] ||
+        field.trim() == ""
+      ) {
+        return HttpResponse.badRequest(field);
+      }
     }
-    if (!password) {
-      return HttpResponse.badRequest("password");
-    }
-    return HttpResponse.ok("");
+
+    this.authService.auth(httpRequest.body);
+    return HttpResponse.ok<string>("");
   }
 }
--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -1,33 +1,49 @@
 import LoginRouter from "./Login-router";
-import { MissingParamError } from "./helpers/Missing-param-error";
+import { MissingParamError } from "../helpers/Missing-param-error";
+import { AuthUserParams } from "./interfaces/authParams";
+import { IAuthService } from "./interfaces/authService";
+
+const makeSut = () => {
+  class AuthService implements IAuthService {
+    async auth(params: AuthUserParams) {
+      return params;
+    }
+  }
+  const authService = new AuthService();
+  const sut = new LoginRouter(authService);
+  return {
+    authService: authService,
+    sut,
+  };
+};
 
 describe("Login Router", () => {
-  it("Should return 400 if no username is provided", () => {
-    const sut = new LoginRouter();
+  it("Should return 400 if no username is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: "any_password",
       },
     };
-    const httpResponse = sut.route(httpRequest);
+    const httpResponse = await sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("username"));
   });
-  it("Should return 400 if no password is provided", () => {
-    const sut = new LoginRouter();
+  it("Should return 400 if no password is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         username: "any_username",
       },
     };
-    const httpResponse = sut.route(httpRequest);
+    const httpResponse = await sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
-  it("Should return 500 if no body is provided", () => {
-    const sut = new LoginRouter();
+  it("Should return 500 if no body is provided", async () => {
+    const { sut } = makeSut();
     const httpRequest = {};
-    const httpResponse = sut.route(httpRequest);
+    const httpResponse = await sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
   });
 });

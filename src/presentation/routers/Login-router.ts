65437871b5d8dcc1489294992f436524ede65ba5
feliperodsdev <libertyisrequired@gmail.com>
2023-01-19 18:40:02 -0300

    fix: test on commit and try catch at LoginRouter

--- src/presentation/routers/Login-router.ts
+++ src/presentation/routers/Login-router.ts
@@ -9,23 +9,27 @@ export default class LoginRouter {
     this.authService = authService;
   }
   async route(httpRequest: any) {
-    if (!httpRequest.body) {
+    try {
+      if (!httpRequest.body) {
+        return HttpResponse.serverError();
+      }
+
+      const requiredFields = ["username", "password"];
+
+      for (const field of requiredFields) {
+        if (
+          !httpRequest?.body?.[field as keyof AuthUserParams] ||
+          field.trim() == ""
+        ) {
+          return HttpResponse.badRequest(field);
+        }
+      }
+
+      const token = await this.authService.auth(httpRequest.body);
+      if (!token.token) return HttpResponse.invalidCredentials();
+      return HttpResponse.ok<IToken>(token);
+    } catch (e) {
       return HttpResponse.serverError();
     }
-
-    const requiredFields = ["username", "password"];
-
-    for (const field of requiredFields) {
-      if (
-        !httpRequest?.body?.[field as keyof AuthUserParams] ||
-        field.trim() == ""
-      ) {
-        return HttpResponse.badRequest(field);
-      }
-    }
-
-    const token = await this.authService.auth(httpRequest.body);
-    if (!token.token) return HttpResponse.invalidCredentials();
-    return HttpResponse.ok<IToken>(token);
   }
 }

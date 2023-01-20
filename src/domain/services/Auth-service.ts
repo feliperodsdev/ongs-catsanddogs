ab12e4b309f27e393ccf1cc4395b2b2a5e93731f
feliperodsdev <libertyisrequired@gmail.com>
2023-01-20 18:22:25 -0300

    feat: move authservice to production

---
+++ src/domain/services/Auth-service.ts
@@ -0,0 +1,44 @@
+import { IAuthUserParams } from "./interfaces/authParams";
+import { IAuthService } from "./interfaces/authService";
+import { IEncrypterPassword } from "./interfaces/encrypterPassword";
+import { ILoadUserByUsernameRepository } from "./interfaces/loadUserByUsernameRepository";
+import { ITokenGenerator } from "./interfaces/tokenGenerator";
+
+export class AuthService implements IAuthService {
+  private encrypterPassword: IEncrypterPassword;
+  private tokenGenerator: ITokenGenerator;
+  private loadUserByUsernameRepository: ILoadUserByUsernameRepository;
+
+  constructor(
+    loadUserByUsernameRepository: ILoadUserByUsernameRepository,
+    encrypterPassword: IEncrypterPassword,
+    tokenGenerator: ITokenGenerator
+  ) {
+    this.loadUserByUsernameRepository = loadUserByUsernameRepository;
+    this.encrypterPassword = encrypterPassword;
+    this.tokenGenerator = tokenGenerator;
+  }
+
+  async auth(params: IAuthUserParams) {
+    const type = "Bearer";
+    const user = await this.loadUserByUsernameRepository.load(params.username);
+    const isValid =
+      user &&
+      (await this.encrypterPassword.compare(params.password, user.password));
+
+    if (isValid) {
+      const token = await this.tokenGenerator.generateToken({
+        user_id: user.id,
+      });
+      return {
+        type: type,
+        token: token,
+      };
+    }
+
+    return {
+      type: null,
+      token: null,
+    };
+  }
+}
--- src/domain/services/auth-service.spec.ts
+++ src/domain/services/auth-service.spec.ts
@@ -1,52 +1,10 @@
-import { IAuthUserParams } from "./interfaces/authParams";
-import { IAuthService } from "./interfaces/authService";
+import { AuthService } from "./Auth-service";
 import { IEncrypterPassword } from "./interfaces/encrypterPassword";
 import { ILoadUserByUsernameRepository } from "./interfaces/loadUserByUsernameRepository";
 import { ITokenGenerator } from "./interfaces/tokenGenerator";
 import { ITokenInfo } from "./interfaces/tokenInfo";
 
 const makeSut = () => {
-  class AuthService implements IAuthService {
-    private loadUserByUsernameRepository: ILoadUserByUsernameRepository;
-    private encrypterPassword: IEncrypterPassword;
-    private tokenGenerator: ITokenGenerator;
-
-    constructor(
-      loadUserByUsernameRepository: ILoadUserByUsernameRepository,
-      encrypterPassword: IEncrypterPassword,
-      tokenGenerator: ITokenGenerator
-    ) {
-      this.loadUserByUsernameRepository = loadUserByUsernameRepository;
-      this.encrypterPassword = encrypterPassword;
-      this.tokenGenerator = tokenGenerator;
-    }
-
-    async auth(params: IAuthUserParams) {
-      const type = "Bearer";
-      const user = await this.loadUserByUsernameRepository.load(
-        params.username
-      );
-      const isValid =
-        user &&
-        (await this.encrypterPassword.compare(params.password, user.password));
-
-      if (isValid) {
-        const token = await this.tokenGenerator.generateToken({
-          user_id: user.id,
-        });
-        return {
-          type: type,
-          token: token,
-        };
-      }
-
-      return {
-        type: null,
-        token: null,
-      };
-    }
-  }
-
   class EncrypterPasswordSpy implements IEncrypterPassword {
     async hash(password: string): Promise<string> {
       return password + "#";

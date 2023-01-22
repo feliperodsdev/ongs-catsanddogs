87aa606b0ed781ff07553771e423a57315b46e80
feliperodsdev <libertyisrequired@gmail.com>
2023-01-22 12:58:38 -0300

    feat: modifying route on test

--- src/main/config/setup.spec.ts
+++ src/main/config/setup.spec.ts
@@ -13,7 +13,7 @@ describe("App Setup", () => {
     app.get("/test_cors", (req, res) => {
       res.send("");
     });
-    const res = await request(app).get("/test");
+    const res = await request(app).get("/test_cors");
     expect(res.headers["access-control-allow-origin"]).toBe("*");
     expect(res.headers["access-control-allow-methods"]).toBe("*");
     expect(res.headers["access-control-allow-headers"]).toBe("*");

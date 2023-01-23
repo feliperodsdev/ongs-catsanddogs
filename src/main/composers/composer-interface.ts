6acafaa4abbd1be0085987ca7e1b909e6510ea1e
liber <libertyisrequired@gmail.com>
2023-01-22 22:01:12 -0300

    feat: composer interface and login router composer

---
+++ src/main/composers/composer-interface.ts
@@ -0,0 +1,3 @@
+export interface IComposer {
+  compose(): any;
+}
---
+++ src/main/composers/login-router-composer.ts
@@ -0,0 +1,28 @@
+import { TokenManager } from "../../utils/helpers/Token-manager";
+import { IComposer } from "./composer-interface";
+import jwt from "jsonwebtoken";
+import { Encrypter } from "../../utils/helpers/Encrypter";
+import bcrypt from "bcryptjs";
+import { LoadUserByUsernameRepository } from "../../infra/repositories/Prisma-Load-User-By-Username";
+import { AuthService } from "../../domain/services/Auth-service";
+import LoginRouter from "../../presentation/routers/Login-router";
+
+const secret =
+  process.env.SECRET_KEY ||
+  "nfiowejbgfiwebgjilerbgjiegreNBERJKLBFJLERVGFHLWEJFBFERJKL";
+
+const keyValue = parseInt(process.env.KEY_VALUE || "FNOSEJNFFKESNFKL") || 8;
+
+export class LoginRouterComposer implements IComposer {
+  compose() {
+    const tokenGenerator = new TokenManager(jwt, secret);
+    const encrypter = new Encrypter(bcrypt, keyValue);
+    const loadUserByUsernameRepository = new LoadUserByUsernameRepository();
+    const authService = new AuthService(
+      loadUserByUsernameRepository,
+      encrypter,
+      tokenGenerator
+    );
+    return new LoginRouter(authService);
+  }
+}

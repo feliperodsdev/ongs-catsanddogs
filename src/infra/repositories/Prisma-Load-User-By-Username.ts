f94912876f6cdf50130517b855449fd2663f32e6
liber <libertyisrequired@gmail.com>
2023-01-22 22:00:00 -0300

    fix: prettier refactoring format

--- src/infra/repositories/Prisma-Load-User-By-Username.ts
+++ src/infra/repositories/Prisma-Load-User-By-Username.ts
@@ -2,7 +2,9 @@ import { prisma } from "../../domain/Prisma-client";
 import { ILoadUserByUsernameRepository } from "./interfaces/loadUserByUsernameRepository";
 import { UserModel } from "../../domain/services/interfaces/userModel";
 
-class LoadUserByUsernameRepository implements ILoadUserByUsernameRepository {
+export class LoadUserByUsernameRepository
+  implements ILoadUserByUsernameRepository
+{
   async load(username: string): Promise<UserModel | null> {
     const user = await prisma.user.findFirst({
       where: {
@@ -12,5 +14,3 @@ class LoadUserByUsernameRepository implements ILoadUserByUsernameRepository {
     return user;
   }
 }
-
-export const loadUserByUsernameRepository = new LoadUserByUsernameRepository();

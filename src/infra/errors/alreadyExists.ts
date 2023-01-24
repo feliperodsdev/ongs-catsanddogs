736443d33e11707e684c246382d02c4fc3e84064
feliperodsdev <libertyisrequired@gmail.com>
2023-01-24 10:41:22 -0300

    fix: name of error

--- src/infra/errors/alreadyExists.ts
+++ src/infra/errors/alreadyExists.ts
@@ -1,4 +1,4 @@
-export class ALreadyExistsError extends Error {
+export class AlreadyExistsError extends Error {
   constructor(param: string) {
     super(`${param} already exists`);
     this.name = "AlreadyExistsError";

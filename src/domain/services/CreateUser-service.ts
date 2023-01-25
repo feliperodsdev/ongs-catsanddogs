90686a49284fe3b2b303a4a78673a9525f6fb9da
feliperodsdev <libertyisrequired@gmail.com>
2023-01-25 10:51:38 -0300

    feat: move files to production

---
+++ src/domain/services/CreateUser-service.ts
@@ -0,0 +1,36 @@
+import { ICreateUserRepository } from "../../infra/repositories/interfaces/createUserRepository";
+import { ILoadUserByUsernameRepository } from "../../infra/repositories/interfaces/loadUserByUsernameRepository";
+import { IEncrypterPassword } from "../../utils/helpers/interfaces/encrypterPassword";
+import { ICreateUserParams } from "./interfaces/createUserParams";
+import { ICreateUserService } from "./interfaces/services/createUserService";
+
+export class CreateUserService implements ICreateUserService {
+  private createUserRepository: ICreateUserRepository;
+  private encrypter: IEncrypterPassword;
+  private loadUserByUsername: ILoadUserByUsernameRepository;
+  constructor(
+    createUserRepository: ICreateUserRepository,
+    encrypter: IEncrypterPassword,
+    loadUserByUsername: ILoadUserByUsernameRepository
+  ) {
+    this.createUserRepository = createUserRepository;
+    this.encrypter = encrypter;
+    this.loadUserByUsername = loadUserByUsername;
+  }
+  async createUser(params: ICreateUserParams): Promise<boolean> {
+    const userAlreadyExists = await this.loadUserByUsername.load(
+      params.username
+    );
+
+    if (!userAlreadyExists) {
+      return false;
+    }
+
+    const userToBeCreated = params;
+    userToBeCreated.password = await this.encrypter.hash(params.password);
+
+    await this.createUserRepository.createUser(params);
+
+    return true;
+  }
+}
--- src/domain/services/create-user-service.spec.ts
+++ src/domain/services/create-user-service.spec.ts
@@ -2,43 +2,12 @@ import { UserModel } from "../../infra/models/userModel";
 import { ICreateUserRepository } from "../../infra/repositories/interfaces/createUserRepository";
 import { ILoadUserByUsernameRepository } from "../../infra/repositories/interfaces/loadUserByUsernameRepository";
 import { IEncrypterPassword } from "../../utils/helpers/interfaces/encrypterPassword";
+import { CreateUserService } from "./CreateUser-service";
 import { ICreateUserParams } from "./interfaces/createUserParams";
-import { ICreateUserService } from "./interfaces/services/createUserService";
 
 const inMemoryDbUser: any[] = [];
 
 const makeSut = () => {
-  class CreateUserService implements ICreateUserService {
-    private createUserRepository: ICreateUserRepository;
-    private encrypter: IEncrypterPassword;
-    private loadUserByUsername: ILoadUserByUsernameRepository;
-    constructor(
-      createUserRepository: ICreateUserRepository,
-      encrypter: IEncrypterPassword,
-      loadUserByUsername: ILoadUserByUsernameRepository
-    ) {
-      this.createUserRepository = createUserRepository;
-      this.encrypter = encrypter;
-      this.loadUserByUsername = loadUserByUsername;
-    }
-    async createUser(params: ICreateUserParams): Promise<boolean> {
-      const userAlreadyExists = await this.loadUserByUsername.load(
-        params.username
-      );
-
-      if (!userAlreadyExists) {
-        return false;
-      }
-
-      const userToBeCreated = params;
-      userToBeCreated.password = await this.encrypter.hash(params.password);
-
-      await this.createUserRepository.createUser(params);
-
-      return true;
-    }
-  }
-
   class LoadUserByUsernameRepositorySpy
     implements ILoadUserByUsernameRepository
   {

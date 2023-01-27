49b2fe26083e1827820c2063bf1ca44d32185421
feliperodsdev <libertyisrequired@gmail.com>
2023-01-27 09:56:30 -0300

    fix: try catch

--- src/presentation/routers/Append-User-Service-Point.ts
+++ src/presentation/routers/Append-User-Service-Point.ts
@@ -4,21 +4,25 @@ import HttpResponse from "../helpers/Http-response";
 export class AppendUserToServicePointRouter {
   constructor(private readonly appendUserToService: IAppendUserToService) {}
   async route(httpRequest: any) {
-    if (!httpRequest.body) {
-      return HttpResponse.badRequest("Body");
+    try {
+      if (!httpRequest.body) {
+        return HttpResponse.badRequest("Body");
+      }
+
+      if (!httpRequest.body["service_point_id"]) {
+        return HttpResponse.badRequest("service_point_id");
+      }
+
+      const userId = httpRequest.user.userId;
+      const data = {
+        service_point_id: httpRequest.body.service_point_id,
+        user_id: userId,
+      };
+      await this.appendUserToService.append(data);
+
+      return HttpResponse.ok("Append");
+    } catch (e) {
+      return HttpResponse.serverError();
     }
-
-    if (!httpRequest.body["service_point_id"]) {
-      return HttpResponse.badRequest("service_point_id");
-    }
-
-    const userId = httpRequest.user.userId;
-    const data = {
-      service_point_id: httpRequest.body.service_point_id,
-      user_id: userId,
-    };
-    await this.appendUserToService.append(data);
-
-    return HttpResponse.ok("Append");
   }
 }

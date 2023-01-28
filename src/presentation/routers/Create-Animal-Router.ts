e10338224906547cd683efa4dbf0feda12e7fff5
liber <libertyisrequired@gmail.com>
2023-01-28 11:51:46 -0300

    feat: move files to production

---
+++ src/presentation/routers/Create-Animal-Router.ts
@@ -0,0 +1,49 @@
+import { ICreateAnAnimalParams } from "../../domain/services/createAnAnimalParams";
+import { ICreateAnAnimalService } from "../../domain/services/interfaces/services/createAnAnimal";
+import HttpResponse from "../helpers/Http-response";
+
+export class CreateAnAnimalRouter {
+  constructor(private readonly createAnAnimalService: ICreateAnAnimalService) {
+    this.createAnAnimalService = createAnAnimalService;
+  }
+  async route(httpRequest: any) {
+    try {
+      if (!httpRequest.body) {
+        return HttpResponse.badRequest("Body");
+      }
+
+      const user = httpRequest.user;
+
+      const requiredFields = [
+        "desc",
+        "name",
+        "specie",
+        "weight",
+        "sickness",
+        "castrated",
+        "breed",
+        "service_point_id",
+        "approxAge",
+      ];
+
+      for (const field of requiredFields) {
+        if (
+          !httpRequest?.body?.[field as keyof ICreateAnAnimalParams] ||
+          field.trim() == ""
+        ) {
+          return HttpResponse.badRequest(field);
+        }
+      }
+      await this.createAnAnimalService.create({
+        ...httpRequest.body,
+        user_id: user.userId,
+      });
+      return HttpResponse.ok("Created");
+    } catch (e: any) {
+      if (e.name == "EntitieValidationError") {
+        return HttpResponse.ok(e.message);
+      }
+      return HttpResponse.serverError();
+    }
+  }
+}
--- src/presentation/routers/create-animal-router.spec.ts
+++ src/presentation/routers/create-animal-router.spec.ts
@@ -1,41 +1,29 @@
+import { Cat } from "../../domain/entities/Cat";
+import { Dog } from "../../domain/entities/Dog";
 import { ICreateAnAnimalParams } from "../../domain/services/createAnAnimalParams";
-import HttpResponse from "../helpers/Http-response";
+import { ICreateAnAnimalService } from "../../domain/services/interfaces/services/createAnAnimal";
+import { CreateAnAnimalRouter } from "./Create-Animal-Router";
 
 const makeSut = () => {
-  class CreateAnAnimalRouter {
-    async route(httpRequest: any) {
-      try {
-        if (!httpRequest.body) {
-          return HttpResponse.badRequest("Body");
-        }
-
-        const user = httpRequest.user;
-
-        const requiredFields = [
-          "desc",
-          "name",
-          "specie",
-          "weight",
-          "sickness",
-          "castraded",
-          "breed",
-        ];
-
-        for (const field of requiredFields) {
-          if (
-            !httpRequest?.body?.[field as keyof ICreateAnAnimalParams] ||
-            field.trim() == ""
-          ) {
-            return HttpResponse.badRequest(field);
-          }
-        }
-      } catch (e) {
-        return HttpResponse.serverError();
+  class CreateAnAnimalServiceSpy implements ICreateAnAnimalService {
+    async create(params: ICreateAnAnimalParams): Promise<void> {
+      let animalToBeCreated;
+      const breed = params.breed;
+      if (params.specie == 1) {
+        animalToBeCreated = new Cat(
+          { ...params, arrivedDate: new Date() },
+          { breed: breed }
+        );
       }
+      animalToBeCreated = new Dog(
+        { ...params, arrivedDate: new Date() },
+        { breed: breed }
+      );
+      console.log(animalToBeCreated);
     }
   }
 
-  const sut = new CreateAnAnimalRouter();
+  const sut = new CreateAnAnimalRouter(new CreateAnAnimalServiceSpy());
 
   return {
     sut,
@@ -53,10 +41,94 @@ describe("CreateAnAnimalRouter", () => {
   });
   it("Should return 400 if no ICreateAnAnimalParams is provided", async () => {
     const { sut } = makeSut();
-    const httpRequest = {};
+    const httpRequest = {
+      body: {
+        name: "Gary",
+        specie: 1,
+        approxAge: 5,
+        sickness: true,
+        castrated: true,
+        arrivedDate: new Date(),
+        weight: 555,
+        breed: "orange cat",
+      },
+    };
 
     const response = await sut.route(httpRequest);
 
     expect(response.statusCode).toEqual(400);
+  });
+  it("Should return 'Specie does not exist' if no valid specie is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      user: {
+        userId: 2,
+      },
+      body: {
+        name: "Gary",
+        specie: 543543,
+        approxAge: 5,
+        sickness: true,
+        castrated: true,
+        arrivedDate: new Date(),
+        weight: 555,
+        desc: "I love animals",
+        breed: "orange cat",
+        service_point_id: 1,
+      },
+    };
+
+    const response = await sut.route(httpRequest);
+    expect(response.body).toEqual({ data: "Specie does not exist." });
+  });
+  it("Should return 'Age cannot be less than or equal to 0.' if no valid age is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      user: {
+        userId: 2,
+      },
+      body: {
+        name: "Gary",
+        specie: 1,
+        approxAge: -1,
+        sickness: true,
+        castrated: true,
+        arrivedDate: new Date(),
+        weight: 555,
+        desc: "I love animals",
+        breed: "orange cat",
+        service_point_id: 1,
+      },
+    };
+
+    const response = await sut.route(httpRequest);
+    expect(response.body).toEqual({
+      data: "Age cannot be less than or equal to 0.",
+    });
+  });
+  it("Should return 'Weight cannot be less than or equal to 0.' if no valid weight is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      user: {
+        userId: 2,
+      },
+      body: {
+        name: "Gary",
+        specie: 1,
+        approxAge: 10,
+        sickness: true,
+        castrated: true,
+        arrivedDate: new Date(),
+        weight: -1,
+        desc: "I love animals",
+        breed: "orange cat",
+        service_point_id: 1,
+      },
+    };
+
+    const response = await sut.route(httpRequest);
+    expect(response.body).toEqual({
+      data: "Weight cannot be less than or equal to 0.",
+    });
   });
 });
